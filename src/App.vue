1211138109ac1c37443f73052877c878c21e69a7
Kevin <1041874421@qq.com>
2017-05-11 12:25:27 +0800

    划清模块 加载scss-loader

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div class="content">
+      I am content
+    </div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import header from "./components/header/header"
 
 export default {
-  name: 'app',
-  components: {
-    Hello
+  components:{
+    "v-header":header
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  #app{
+    .tab{
+      display: flex;
+      width:100%;
+      height:40px;
+      line-height:40px;
+      .tab-item{
+        flex:1;
+        text-align: center;
+      }
+    }
+  }
 </style>
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,9 +1,13 @@
 <template>  
-
+    <div class="header">
+        我是Header
+    </div>
 </template>
 <script>
+    export default{
 
+    }
 </script>
 <style>
-    
+
 </style>
