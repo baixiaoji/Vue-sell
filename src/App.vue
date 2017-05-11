d5e7776668ec28bd560f00312f9698b980e75a53
Kevin <1041874421@qq.com>
2017-05-11 14:05:07 +0800

    引入vue-router 遇见BUG 1.不渲染页面 解决： 1.增加一个render函数，但不清楚为什么

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,17 @@
   <div id="app">
     <v-header></v-header>
     <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评论</div>
-      <div class="tab-item">商家</div>
+      <div class="tab-item">
+         <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
     </div>
-    <div class="content">
-      I am content
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,41 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueRouter from "vue-router"
 import App from './App'
 
-Vue.config.productionTip = false
+import goods from "./components/goods/goods"
+import ratings from "./components/ratings/ratings"
+import seller from "./components/seller/seller"
 
+Vue.config.productionTip = false
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+// new Vue({
+//   el: '#app',
+//   template: '<App/>',
+//   components: { App }
+// })
+
+Vue.use(VueRouter)
+
+
+const routes = [
+  { path: '/goods', component: goods },
+  { path: '/ratings', component: ratings },
+  { path: '/seller', component: seller },
+]
+
+const router = new VueRouter({
+  routes 
 })
+
+// 4. 创建和挂载根实例。
+// 记得要通过 router 配置参数注入路由，
+// 从而让整个应用都有路由功能
+// let app = Vue.extend(App)
+const app = new Vue({
+  // el: "#app",
+  router,
+  render: h => h(App)
+}).$mount('#app')
+
