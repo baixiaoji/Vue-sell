620ce730370003cd7b250cfd054c000f12ba5f30
Kevin <1041874421@qq.com>
2017-05-15 09:21:56 +0800

    finish the basic shopcart

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,8 +1,8 @@
-<template>  
+<template>
     <div class="shopcart">
         <div class="content">
             <div class="content-left">
-                <div class="logo-wrapper" >
+                <div class="logo-wrapper">
                     <div class="logo" :class="{'highlight':totalCount>0}">
                         <span class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></span>
                     </div>
@@ -12,94 +12,118 @@
                 <div class="desc">另需配送费￥{{deliverPrice}}</div>
             </div>
             <div class="content-right">
-                <div class="pay">￥{{minPrice}}元起送</div>
+                <div class="pay" :class="payClass">{{payDesc}}</div>
             </div>
         </div>
     </div>
 </template>
 <script>
-    export default{
-        props:{
-            selectFoods:{
+    export default {
+        props: {
+            selectFoods: {
                 type: Array,
-                default(){
-                    return [ ];
+                default() {
+                    return [
+                        {
+                            price: 10,
+                            count: 3
+                        }
+                    ];
                 }
             },
-            deliverPrice:{
-                type:Number,
+            deliverPrice: {
+                type: Number,
                 default: 0
             },
-            minPrice:{
+            minPrice: {
                 type: Number,
                 default: 0
             }
         },
-        computed:{
-            totalPrice(){
+        computed: {
+            totalPrice() {
                 let total = 0;
-                this.selectFoods.forEach((food)=>{
+                this.selectFoods.forEach((food) => {
                     total += food.price * food.count
                 })
 
                 return total
             },
-            totalCount(){
+            totalCount() {
                 let count = 0;
-                this.selectFoods.forEach((food)=>{
-                    count +=  food.count
+                this.selectFoods.forEach((food) => {
+                    count += food.count
                 })
 
                 return count
+            },
+            payDesc() {
+                if (this.totalPrice === 0) {
+                    return `￥${this.minPrice}元起送`
+                }else if(this.totalPrice < this.minPrice){
+                    let diff  = this.minPrice - this.totalPrice;
+                    return `还差￥${diff}元`
+                }else{
+                    return "去结算"
+                }
+            },
+            payClass() {
+                // 计算class 
+                if(this.totalPrice < this.minPrice){
+                    return "not-enough"
+                }else{
+                    return "enough"
+                }
             }
         }
     }
+
 </script>
 <style lang="scss">
-    .shopcart{
+    .shopcart {
         position: fixed;
         left: 0;
         bottom: 0;
         z-index: 10;
         width: 100%;
         height: 48px;
-        .content{
+        .content {
             display: flex;
             background: #141d27;
             font-size: 0;
-            .content-left{
+            .content-left {
                 flex: 1;
-                .logo-wrapper{
+                .logo-wrapper {
                     display: inline-block;
-                    position:relative;
+                    position: relative;
                     top: -10px;
-                    margin:0 12px;
+                    margin: 0 12px;
                     padding: 6px;
-                    width:56px;
+                    width: 56px;
                     height: 56px;
                     box-sizing: border-box;
                     vertical-align: top;
                     border-radius: 50%;
                     background: #141d27;
-                    .logo{
-                        width:100%;
+                    .logo {
+                        width: 100%;
                         height: 100%;
-                        border-radius:50%;
-                        background:#2b343c;
-                        text-align:center;
-                        &.highlight{
-                            background: rgb(0,160,220);
+                        border-radius: 50%;
+                        background: #2b343c;
+                        text-align: center;
+                        &.highlight {
+                            background: rgb(0, 160, 220);
                         }
-                        .icon-shopping_cart{
+                        .icon-shopping_cart {
                             font-size: 24px;
                             color: #80858a;
-                            line-height:44px;
-                            &.highlight{
+                            line-height: 44px;
+                            &.highlight {
                                 color: #fff;
                             }
                         }
                     }
-                    .num{
+                    .num {
                         position: absolute;
                         top: 0;
                         right: 0;
@@ -110,46 +134,52 @@
                         border-radius: 16px;
                         font-size: 9px;
                         font-weight: 700;
-                        color:#fff;
-                        background:rgb(240,20,20);
-                        box-shadow: 0 4px 8px 0 rgba(0,0,0,.4);
+                        color: #fff;
+                        background: rgb(240, 20, 20);
+                        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, .4);
                     }
                 }
-                .price{
-                     display: inline-block;
-                     vertical-align: top;
-                     line-height: 24px;
-                     margin-top: 12px;
-                     padding-right: 12px;
-                     box-sizing: border-box;
-                     border-right: 1px solid rgba(255,255,255,.1);
-                     font-size: 16px;
-                     font-weight: 700;
-                     color: rgba(255,255,255,.4);
-                     &.highlight{
+                .price {
+                    display: inline-block;
+                    vertical-align: top;
+                    line-height: 24px;
+                    margin-top: 12px;
+                    padding-right: 12px;
+                    box-sizing: border-box;
+                    border-right: 1px solid rgba(255, 255, 255, .1);
+                    font-size: 16px;
+                    font-weight: 700;
+                    color: rgba(255, 255, 255, .4);
+                    &.highlight {
                         color: #fff;
                     }
                 }
-                .desc{
-                     display: inline-block;
-                     vertical-align: top;
-                     line-height: 24px;
-                     margin: 12px 0 0 12px;
-                     font-size: 10px;
-                     color: rgba(255,255,255,.4);
-
+                .desc {
+                    display: inline-block;
+                    vertical-align: top;
+                    line-height: 24px;
+                    margin: 12px 0 0 12px;
+                    font-size: 10px;
+                    color: rgba(255, 255, 255, .4);
                 }
             }
-            .content-right{
+            .content-right {
                 flex: 0 0 105px;
                 width: 105px;
-                .pay{
+                .pay {
                     height: 48px;
                     line-height: 48px;
                     text-align: center;
-                    font-size:  12px;
-                    color: rgba(255,255,255,.4);
+                    font-size: 12px;
+                    color: rgba(255, 255, 255, .4);
                     background: #2b333b;
+                    &.not-enough{
+                        background: #2b333b;
+                    }
+                    &.enough{
+                        background: #00b43c;
+                        color: #fff;
+                    }
                 }
             }
         }
